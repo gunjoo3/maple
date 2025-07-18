ef5bed4f17e3332ac7424e5e59aa2d528192cb32
gunjoo3 <kcert9784@gmail.com>
2025-07-18 23:35:24 +0900

    Update songs.js

--- scripts/songs.js
+++ scripts/songs.js
@@ -1,336 +1,12 @@
 let songs = [
   {
-    name: "Night Market",
-    cover:
-      "앨범/1.gif",
-    artist: "세계여행 대만",
-    audio: "./노래/1.mp3",
-    id: 0,
-    number: "1 / 61",
-  },
-  {
-    name: "Ludibrium: Fantasia",
-    cover:
-      "앨범/2.gif",
-    artist: "루디브리엄 알리샤르 파티퀘스트",
-    audio: "./노래/2.mp3",
-    id: 1,
-    number: "2 / 61",
-  },
-  {
-    name: "Orbis: Tower of Goddess",
-    cover:
-      "앨범/3.gif",
-    artist: "오르비스 여신의 흔적 파티퀘스트",
-    audio: "./노래/3.mp3",
-    id: 2,
-    number: "3 / 61",
-  },
-  {
-    name: "Amorian Challenge",
-    cover:
-      "앨범/4.gif",
-    artist: "결혼 시스템 파티퀘스트",
-    audio: "./노래/4.mp3",
-    id: 3,
-    number: "4 / 61",
-  },
-  {
-    name: "Ellinia: Missing You",
-    cover:
-      "앨범/5.gif",
-    artist: "엘리니아 사냥터",
-    audio: "./노래/5.mp3",
-    id: 4,
-    number: "5 / 61",
-  },
-  {
-    name: "Ludibrium: Flying in a Blue Dream",
-    cover:
-      "앨범/6.gif",
-    artist: "루디브리엄 행",
-    audio: "./노래/6.mp3",
-    id: 5,
-    number: "6 / 61",
-  },
-  {
-    name: "Aqua Road: Blue World",
-    cover:
-      "앨범/7.gif",
-    artist: "아쿠아리움 사냥터",
-    audio: "./노래/7.mp3",
-    id: 6,
-    number: "7 / 61",
-  },
-  {
-    name: "Aqua Road: Shining Sea",
-    cover:
-      "앨범/8.gif",
-    artist: "아쿠아리움 사냥터",
-    audio: "./노래/8.mp3",
-    id: 7,
-    number: "8 / 61",
-  },
-  {
-    name: "Coke Town",
-    cover:
-      "앨범/9.gif",
-    artist: "코크타운",
-    audio: "./노래/9.mp3",
-    id: 8,
-    number: "9 / 61",
-  },
-  {
-    name: "Ereve: Raindrop Flower",
-    cover:
-      "앨범/10.JPG",
-    artist: "에레브 사냥터",
-    audio: "./노래/10.mp3",
-    id: 9,
-    number: "10 / 61",
-  },
-  {
-    name: "Ludibrium: Wherever You Are",
-    cover:
-      "앨범/12.gif",
-    artist: "루디브리엄 사냥터",
-    audio: "./노래/12.mp3",
-    id: 10,
-    number: "11 / 61",
-  },
-  {
-    name: "Orbis: Shinin' Harbor",
-    cover:
-      "앨범/13.gif",
-    artist: "오르비스",
-    audio: "./노래/13.mp3",
-    id: 11,
-    number: "12 / 61",
-  },
-  {
-    name: "Orbis: Come with Me",
-    cover:
-      "앨범/14.gif",
-    artist: "오르비스",
-    audio: "./노래/14.mp3",
-    id: 12,
-    number: "13 / 61",
-  },
-  {
-    name: "China: Go Shanghai",
-    cover:
-      "앨범/15.gif",
-    artist: "세계여행 중국",
-    audio: "./노래/15.mp3",
-    id: 13,
-    number: "14 / 61",
-  },
-  {
-    name: "El Nath: Warm Regard",
-    cover:
-      "앨범/16.JPG",
-    artist: "엘나스 사냥터",
-    audio: "./노래/16.mp3",
-    id: 14,
-    number: "15 / 61",
-  },
-  {
-    name: "Orbis: Upon the Sky",
-    cover:
-      "앨범/17.gif",
-    artist: "오르비스 행",
-    audio: "./노래/17.mp3",
-    id: 15,
-    number: "16 / 61",
-  },
-  {
-    name: "Ludibrium: Fairytale",
-    cover:
-      "앨범/18.JPG",
-    artist: "루디브리엄 사냥터",
-    audio: "./노래/18.mp3",
-    id: 16,
-    number: "17 / 61",
-  },
-  {
-    name: "Ludibrium: Timeless",
-    cover:
-      "앨범/18.JPG",
-    artist: "루디브리엄 사냥터",
-    audio: "./노래/26.mp3",
-    id: 17,
-    number: "18 / 61",
-  },
-  {
-    name: "Ludibrium: Funny Time Maker",
-    cover:
-      "앨범/19.gif",
-    artist: "루디브리엄 에오스탑",
-    audio: "./노래/19.mp3",
-    id: 18,
-    number: "19 / 61",
-  },
-  {
-    name: "Ludibrium: Waltz for Work",
-    cover:
-      "앨범/20.gif",
-    artist: "루디브리엄 사냥터",
-    audio: "./노래/20.mp3",
-    id: 19,
-    number: "20 / 61",
-  },
-  {
-    name: "Ludibrium: High Enough",
-    cover:
-      "앨범/21.JPG",
-    artist: "루디브리엄 사냥터",
-    audio: "./노래/21.mp3",
-    id: 20,
-    number: "21 / 61",
-  },
-  {
-    name: "Pantheon",
-    cover:
-      "앨범/22.jpg",
-    artist: "판테온 마을",
-    audio: "./노래/22.mp3",
-    id: 21,
-    number: "22 / 61",
-  },
-  {
-    name: "Pantheon Field",
-    cover:
-      "앨범/23.JPG",
-    artist: "판테온 사냥터",
-    audio: "./노래/23.mp3",
-    id: 22,
-    number: "23 / 61",
-  },
-  {
-    name: "Pantheon Field",
-    cover:
-      "앨범/61.JPG",
-    artist: "판테온 마을",
-    audio: "./노래/61.mp3",
-    id: 23,
-    number: "24 / 61",
-  },
-  {
-    name: "mine entrance",
-    cover:
-      "앨범/24.JPG",
-    artist: "에델슈타인 사냥터",
-    audio: "./노래/24.mp3",
-    id: 24,
-    number: "25 / 61",
-  },
-  {
-    name: "Ariant: Sunset Desert",
-    cover:
-      "앨범/25.gif",
-    artist: "아리안트 사냥터",
-    audio: "./노래/25.mp3",
-    id: 25,
-    number: "26 / 61",
-  },
-  {
-    name: "Korean Folk: Town Dark Mountain",
-    cover:
-      "앨범/27.gif",
-    artist: "월묘 파티 퀘스트",
-    audio: "./노래/27.mp3",
-    id: 26,
-    number: "27 / 61",
-  },
-  {
-    name: "Temple of Time Remembrance",
-    cover:
-      "앨범/28.JPG",
-    artist: "시간의 신전 사냥터",
-    audio: "./노래/28.mp3",
-    id: 27,
-    number: "28 / 61",
-  },
-  {
-    name: "Crystal Garden",
-    cover:
-      "앨범/29.JPG",
-    artist: "직업 팬텀의 크리스탈 가든",
-    audio: "./노래/29.mp3",
-    id: 28,
-    number: "29 / 61",
-  },
-  {
-    name: "Singapore: Boat Quay Town",
-    cover:
-      "앨범/30.gif",
-    artist: "세계여행 싱가포르 (한국미출시)",
-    audio: "./노래/30.mp3",
-    id: 29,
-    number: "30 / 61",
-  },
-  {
-    name: "China: Shanghai Field",
-    cover:
-      "앨범/31.JPG",
-    artist: "세계여행 중국 사냥터",
-    audio: "./노래/31.mp3",
-    id: 30,
-    number: "31 / 61",
-  },
-  {
-    name: "Herb Town: White Herb",
-    cover:
-      "앨범/32.JPG",
-    artist: "백초마을",
-    audio: "./노래/32.mp3",
-    id: 31,
-    number: "32 / 61",
-  },
-  {
-    name: "Mushroom Castle:  Blue Sky",
-    cover:
-      "앨범/33.JPG",
-    artist: "니은 숲",
-    audio: "./노래/33.mp3",
-    id: 32,
-    number: "33 / 61",
-  },
-  {
-    name: "Mushroom Shrine Feeling",
-    cover:
-      "앨범/34.JPG",
-    artist: "세계여행 일본",
-    audio: "./노래/34.mp3",
-    id: 33,
-    number: "34 / 61",
-  },
-  {
-    name: "Sellas: Where Stars Rest",
-    cover:
-      "앨범/35.jpg",
-    artist: "셀라스 마을 (구.ver)",
-    audio: "./노래/35.mp3",
-    id: 34,
-    number: "35 / 61",
-  },
-  {
-    name: "Omega Sector: For the Glory",
-    cover:
-      "앨범/39.jpg",
-    artist: "지구방위본부대 : 격납고",
-    audio: "./노래/39.mp3",
-    id: 36,
-    number: "36 / 61",
-  },
-   {
     name: "Elin Forest",
     cover:
       "앨범/100.gif",
     artist: "엘린숲",
     audio: "./노래/100.mp3",
     id: 100,
-    number: "37 / 61",
+    number: "1 / 59",
   },
   {
     name: "Ariant: Field",
@@ -339,7 +15,7 @@ let songs = [
     artist: "아리안트 필드",
     audio: "./노래/아리안트 필드.mp3",
     id: 102,
-    number: "38 / 61",
+    number: "2 / 59",
   },
       {
     name: "lien: Field",
@@ -348,7 +24,7 @@ let songs = [
     artist: "리엔필드",
     audio: "./노래/리엔 필드.mp3",
     id: 103,
-    number: "39 / 61",
+    number: "3 / 59",
   },
       {
     name: "노틸러스 필드",
@@ -357,7 +33,7 @@ let songs = [
     artist: "노틸러스",
     audio: "./노래/노틸러스.mp3",
     id: 104,
-    number: "40 / 61",
+    number: "4 / 59",
   },
       {
     name: "요정학원 엘리넬_전체",
@@ -366,16 +42,7 @@ let songs = [
     artist: "요정학원 엘리넬",
     audio: "./노래/요정학원 엘리넬_전체.mp3",
     id: 105,
-    number: "41 / 61",
-  },
-      {
-    name: "천수각",
-    cover:
-      "앨범/천수각.png",
-    artist: "세계여행마을 일본 중",
-    audio: "./노래/천수각.mp3",
-    id: 106,
-    number: "42 / 61",
+    number: "5 / 59",
   },
       {
     name: "무릉도원: 필드",
@@ -384,7 +51,7 @@ let songs = [
     artist: "무릉도원",
     audio: "./노래/무릉필드.mp3",
     id: 107,
-    number: "43 / 61",
+    number: "6 / 59",
   },
   {
     name: "무릉도원: 마을",
@@ -393,7 +60,7 @@ let songs = [
     artist: "무릉도원",
     audio: "./노래/무릉마을.mp3",
     id: 108,
-    number: "44 / 61",
+    number: "7 / 59",
   },
       
       {
@@ -403,7 +70,7 @@ let songs = [
     artist: "크리세",
     audio: "./노래/크리세_전체.mp3",
     id: 109,
-    number: "45 / 61",
+    number: "8 / 59",
   },
       {
     name: "에델슈타인: 필드",
@@ -412,7 +79,7 @@ let songs = [
     artist: "에델슈타인",
     audio: "./노래/에델슈타인 공원.mp3",
     id: 110,
-    number: "46 / 61",
+    number: "9 / 59",
   },
       {
     name: "에델슈타인: 마을",
@@ -421,7 +88,7 @@ let songs = [
     artist: "에델슈타인",
     audio: "./노래/에델슈타인 마을.mp3",
     id: 111,
-    number: "47 / 61",
+    number: "10 / 59",
   },
       {
     name: "에우렐",
@@ -430,7 +97,7 @@ let songs = [
     artist: "에우렐",
     audio: "./노래/에우렐.mp3",
     id: 112,
-    number: "48 / 61",
+    number: "11 / 59",
   },
       {
     name: "슬리피우드 던전",
@@ -439,7 +106,7 @@ let songs = [
     artist: "슬리피우드 던전",
     audio: "./노래/슬리피우드던전.mp3",
     id: 113,
-    number: "49 / 61",
+    number: "12 / 59",
   },
       {
     name: "Gold Beach",
@@ -448,7 +115,7 @@ let songs = [
     artist: "골드비치",
     audio: "./노래/골드비치.mp3",
     id: 114,
-    number: "50 / 61",
+    number: "13 / 59",
   },
       {
     name: "엘레니아: 필드",
@@ -457,7 +124,7 @@ let songs = [
     artist: "엘레니아",
     audio: "./노래/엘리이나필드.mp3",
     id: 115,
-    number: "51 / 61",
+    number: "14 / 59",
   },
       {
     name: "엘레니아: 마을",
@@ -466,7 +133,7 @@ let songs = [
     artist: "엘레니아",
     audio: "./노래/엘리니아마을.mp3",
     id: 116,
-    number: "52 / 61",
+    number: "15 / 59",
   },
       {
     name: "늪지대",
@@ -475,7 +142,7 @@ let songs = [
     artist: "커닝시티",
     audio: "./노래/늪지대.mp3",
     id: 117,
-    number: "53 / 61",
+    number: "16 / 59",
   },
       {
     name: "헤네시스: 전체",
@@ -484,7 +151,7 @@ let songs = [
     artist: "헤네시스",
     audio: "./노래/헤네시스_전체.mp3",
     id: 118,
-    number: "54 / 61",
+    number: "17 / 59",
   },
       {
     name: "리스항구",
@@ -493,7 +160,7 @@ let songs = [
     artist: "리스항구",
     audio: "./노래/리스항구.mp3",
     id: 119,
-    number: "55 / 61",
+    number: "18 / 59",
   },
       {
     name: "에레브: 전체",
@@ -502,7 +169,7 @@ let songs = [
     artist: "에레브",
     audio: "./노래/에레브_전체.mp3",
     id: 120,
-    number: "56 / 61",
+    number: "19 / 59",
   },
       {
     name: "엘나스: 마을",
@@ -511,7 +178,7 @@ let songs = [
     artist: "엘나스",
     audio: "./노래/엘나스.mp3",
     id: 121,
-    number: "57 / 61",
+    number: "20 / 59",
   },
       {
     name: "켄타우로스의 영역",
@@ -520,7 +187,7 @@ let songs = [
     artist: "에레브",
     audio: "./노래/켄타우로스의 영역.mp3",
     id: 122,
-    number: "58 / 61",
+    number: "21 / 59",
   },
       {
     name: "루디브리엄: 마을",
@@ -529,7 +196,7 @@ let songs = [
     artist: "루디브리엄",
     audio: "./노래/루디브리엄.mp3",
     id: 123,
-    number: "59 / 61",
+    number: "22 / 59",
   },
   {
     name: "유적발굴지",
@@ -538,7 +205,322 @@ let songs = [
     artist: "페리온",
     audio: "./노래/군영.mp3",
     id: 124,
-    number: "60 / 61",
+    number: "23 / 59",
+  },
+  {
+    name: "Night Market",
+    cover:
+      "앨범/1.gif",
+    artist: "세계여행 대만",
+    audio: "./노래/1.mp3",
+    id: 0,
+    number: "24 / 59",
+  },
+  {
+    name: "Ludibrium: Fantasia",
+    cover:
+      "앨범/2.gif",
+    artist: "루디브리엄 알리샤르 파티퀘스트",
+    audio: "./노래/2.mp3",
+    id: 1,
+    number: "25 / 59",
+  },
+  {
+    name: "Orbis: Tower of Goddess",
+    cover:
+      "앨범/3.gif",
+    artist: "오르비스 여신의 흔적 파티퀘스트",
+    audio: "./노래/3.mp3",
+    id: 2,
+    number: "26 / 59",
+  },
+  {
+    name: "Amorian Challenge",
+    cover:
+      "앨범/4.gif",
+    artist: "결혼 시스템 파티퀘스트",
+    audio: "./노래/4.mp3",
+    id: 3,
+    number: "27 / 59",
+  },
+  {
+    name: "Ellinia: Missing You",
+    cover:
+      "앨범/5.gif",
+    artist: "엘리니아 사냥터",
+    audio: "./노래/5.mp3",
+    id: 4,
+    number: "28 / 59",
+  },
+  {
+    name: "Ludibrium: Flying in a Blue Dream",
+    cover:
+      "앨범/6.gif",
+    artist: "루디브리엄 행",
+    audio: "./노래/6.mp3",
+    id: 5,
+    number: "29 / 59",
+  },
+  {
+    name: "Aqua Road: Blue World",
+    cover:
+      "앨범/7.gif",
+    artist: "아쿠아리움 사냥터",
+    audio: "./노래/7.mp3",
+    id: 6,
+    number: "30 / 59",
+  },
+  {
+    name: "Aqua Road: Shining Sea",
+    cover:
+      "앨범/8.gif",
+    artist: "아쿠아리움 사냥터",
+    audio: "./노래/8.mp3",
+    id: 7,
+    number: "31 / 59",
+  },
+  {
+    name: "Coke Town",
+    cover:
+      "앨범/9.gif",
+    artist: "코크타운",
+    audio: "./노래/9.mp3",
+    id: 8,
+    number: "32 / 59",
+  },
+  {
+    name: "Ludibrium: Wherever You Are",
+    cover:
+      "앨범/12.gif",
+    artist: "루디브리엄 사냥터",
+    audio: "./노래/12.mp3",
+    id: 10,
+    number: "33 / 59",
+  },
+  {
+    name: "Orbis: Shinin' Harbor",
+    cover:
+      "앨범/13.gif",
+    artist: "오르비스",
+    audio: "./노래/13.mp3",
+    id: 11,
+    number: "34 / 59",
+  },
+  {
+    name: "Orbis: Come with Me",
+    cover:
+      "앨범/14.gif",
+    artist: "오르비스",
+    audio: "./노래/14.mp3",
+    id: 12,
+    number: "35 / 59",
+  },
+  {
+    name: "China: Go Shanghai",
+    cover:
+      "앨범/15.gif",
+    artist: "세계여행 중국",
+    audio: "./노래/15.mp3",
+    id: 13,
+    number: "36 / 59",
+  },
+  {
+    name: "El Nath: Warm Regard",
+    cover:
+      "앨범/16.JPG",
+    artist: "엘나스 사냥터",
+    audio: "./노래/16.mp3",
+    id: 14,
+    number: "37 / 59",
+  },
+  {
+    name: "Orbis: Upon the Sky",
+    cover:
+      "앨범/17.gif",
+    artist: "오르비스 행",
+    audio: "./노래/17.mp3",
+    id: 15,
+    number: "38 / 59",
+  },
+  {
+    name: "Ludibrium: Fairytale",
+    cover:
+      "앨범/18.JPG",
+    artist: "루디브리엄 사냥터",
+    audio: "./노래/18.mp3",
+    id: 16,
+    number: "39 / 59",
+  },
+  {
+    name: "Ludibrium: Timeless",
+    cover:
+      "앨범/18.JPG",
+    artist: "루디브리엄 사냥터",
+    audio: "./노래/26.mp3",
+    id: 17,
+    number: "40 / 59",
+  },
+  {
+    name: "Ludibrium: Funny Time Maker",
+    cover:
+      "앨범/19.gif",
+    artist: "루디브리엄 에오스탑",
+    audio: "./노래/19.mp3",
+    id: 18,
+    number: "41 / 59",
+  },
+  {
+    name: "Ludibrium: Waltz for Work",
+    cover:
+      "앨범/20.gif",
+    artist: "루디브리엄 사냥터",
+    audio: "./노래/20.mp3",
+    id: 19,
+    number: "42 / 59",
+  },
+  {
+    name: "Ludibrium: High Enough",
+    cover:
+      "앨범/21.JPG",
+    artist: "루디브리엄 사냥터",
+    audio: "./노래/21.mp3",
+    id: 20,
+    number: "43 / 59",
+  },
+  {
+    name: "Pantheon",
+    cover:
+      "앨범/22.jpg",
+    artist: "판테온 마을",
+    audio: "./노래/22.mp3",
+    id: 21,
+    number: "44 / 59",
+  },
+  {
+    name: "Pantheon Field",
+    cover:
+      "앨범/23.JPG",
+    artist: "판테온 사냥터",
+    audio: "./노래/23.mp3",
+    id: 22,
+    number: "45 / 59",
+  },
+  {
+    name: "Pantheon Field",
+    cover:
+      "앨범/59.JPG",
+    artist: "판테온 마을",
+    audio: "./노래/59.mp3",
+    id: 23,
+    number: "46 / 59",
+  },
+  {
+    name: "mine entrance",
+    cover:
+      "앨범/24.JPG",
+    artist: "에델슈타인 사냥터",
+    audio: "./노래/24.mp3",
+    id: 24,
+    number: "47 / 59",
+  },
+  {
+    name: "Ariant: Sunset Desert",
+    cover:
+      "앨범/25.gif",
+    artist: "아리안트 사냥터",
+    audio: "./노래/25.mp3",
+    id: 25,
+    number: "48 / 59",
+  },
+  {
+    name: "Korean Folk: Town Dark Mountain",
+    cover:
+      "앨범/27.gif",
+    artist: "월묘 파티 퀘스트",
+    audio: "./노래/27.mp3",
+    id: 26,
+    number: "49 / 59",
+  },
+  {
+    name: "Temple of Time Remembrance",
+    cover:
+      "앨범/28.JPG",
+    artist: "시간의 신전 사냥터",
+    audio: "./노래/28.mp3",
+    id: 27,
+    number: "50 / 59",
+  },
+  {
+    name: "Crystal Garden",
+    cover:
+      "앨범/29.JPG",
+    artist: "직업 팬텀의 크리스탈 가든",
+    audio: "./노래/29.mp3",
+    id: 28,
+    number: "51 / 59",
+  },
+  {
+    name: "Singapore: Boat Quay Town",
+    cover:
+      "앨범/30.gif",
+    artist: "세계여행 싱가포르 (한국미출시)",
+    audio: "./노래/30.mp3",
+    id: 29,
+    number: "52 / 59",
+  },
+  {
+    name: "China: Shanghai Field",
+    cover:
+      "앨범/31.JPG",
+    artist: "세계여행 중국 사냥터",
+    audio: "./노래/31.mp3",
+    id: 30,
+    number: "53 / 59",
+  },
+  {
+    name: "Herb Town: White Herb",
+    cover:
+      "앨범/32.JPG",
+    artist: "백초마을",
+    audio: "./노래/32.mp3",
+    id: 31,
+    number: "54 / 59",
+  },
+  {
+    name: "Mushroom Castle:  Blue Sky",
+    cover:
+      "앨범/33.JPG",
+    artist: "니은 숲",
+    audio: "./노래/33.mp3",
+    id: 32,
+    number: "55 / 59",
+  },
+  {
+    name: "Mushroom Shrine Feeling",
+    cover:
+      "앨범/34.JPG",
+    artist: "세계여행 일본",
+    audio: "./노래/34.mp3",
+    id: 33,
+    number: "56 / 59",
+  },
+  {
+    name: "Sellas: Where Stars Rest",
+    cover:
+      "앨범/35.jpg",
+    artist: "셀라스 마을 (구.ver)",
+    audio: "./노래/35.mp3",
+    id: 34,
+    number: "57 / 59",
+  },
+  {
+    name: "Omega Sector: For the Glory",
+    cover:
+      "앨범/39.jpg",
+    artist: "지구방위본부대 : 격납고",
+    audio: "./노래/39.mp3",
+    id: 36,
+    number: "58 / 59",
   },
  {
     name: "Maple Story Login",
@@ -547,7 +529,7 @@ let songs = [
     artist: "로그인 화면 (구.ver)",
     audio: "./노래/37.mp3",
     id: 37,
-    number: "61 / 61",
+    number: "59 / 59",
   }
   
 ];
