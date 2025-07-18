252bfac06bb061cbcdd9d8c02baa9d288665f0fa
gunjoo3 <kcert9784@gmail.com>
2025-07-18 23:48:00 +0900

    Update songs.js

--- scripts/songs.js
+++ scripts/songs.js
@@ -5,7 +5,7 @@ let songs = [
       "앨범/100.gif",
     artist: "엘린숲",
     audio: "./노래/100.mp3",
-    id: 100,
+    id: 1,
     number: "1 / 59",
   },
   {
@@ -14,7 +14,7 @@ let songs = [
       "앨범/25.gif",
     artist: "아리안트 필드",
     audio: "./노래/아리안트 필드.mp3",
-    id: 102,
+    id: 2,
     number: "2 / 59",
   },
       {
@@ -23,7 +23,7 @@ let songs = [
       "앨범/리엔.png",
     artist: "리엔필드",
     audio: "./노래/리엔 필드.mp3",
-    id: 103,
+    id: 3,
     number: "3 / 59",
   },
       {
@@ -32,7 +32,7 @@ let songs = [
       "앨범/노틸러스.png",
     artist: "노틸러스",
     audio: "./노래/노틸러스.mp3",
-    id: 104,
+    id: 4,
     number: "4 / 59",
   },
       {
@@ -41,7 +41,7 @@ let songs = [
       "앨범/요정학원.png",
     artist: "요정학원 엘리넬",
     audio: "./노래/요정학원 엘리넬_전체.mp3",
-    id: 105,
+    id: 5,
     number: "5 / 59",
   },
       {
@@ -50,7 +50,7 @@ let songs = [
       "앨범/무릉.png",
     artist: "무릉도원",
     audio: "./노래/무릉필드.mp3",
-    id: 107,
+    id: 6,
     number: "6 / 59",
   },
   {
@@ -59,7 +59,7 @@ let songs = [
       "앨범/무릉.png",
     artist: "무릉도원",
     audio: "./노래/무릉마을.mp3",
-    id: 108,
+    id: 7,
     number: "7 / 59",
   },
       
@@ -69,7 +69,7 @@ let songs = [
       "앨범/크리세.png",
     artist: "크리세",
     audio: "./노래/크리세_전체.mp3",
-    id: 109,
+    id: 8,
     number: "8 / 59",
   },
       {
@@ -78,7 +78,7 @@ let songs = [
       "앨범/에델슈타인.png",
     artist: "에델슈타인",
     audio: "./노래/에델슈타인 공원.mp3",
-    id: 110,
+    id: 9,
     number: "9 / 59",
   },
       {
@@ -87,7 +87,7 @@ let songs = [
       "앨범/에델슈타인.png",
     artist: "에델슈타인",
     audio: "./노래/에델슈타인 마을.mp3",
-    id: 111,
+    id: 10,
     number: "10 / 59",
   },
       {
@@ -96,7 +96,7 @@ let songs = [
       "앨범/에우렐.png",
     artist: "에우렐",
     audio: "./노래/에우렐.mp3",
-    id: 112,
+    id: 11,
     number: "11 / 59",
   },
       {
@@ -105,7 +105,7 @@ let songs = [
       "앨범/슬리피.png",
     artist: "슬리피우드 던전",
     audio: "./노래/슬리피우드던전.mp3",
-    id: 113,
+    id: 12,
     number: "12 / 59",
   },
       {
@@ -114,7 +114,7 @@ let songs = [
       "앨범/골드비치.png",
     artist: "골드비치",
     audio: "./노래/골드비치.mp3",
-    id: 114,
+    id: 13,
     number: "13 / 59",
   },
       {
@@ -123,7 +123,7 @@ let songs = [
       "앨범/엘리니아.png",
     artist: "엘레니아",
     audio: "./노래/엘리이나필드.mp3",
-    id: 115,
+    id: 14,
     number: "14 / 59",
   },
       {
@@ -132,7 +132,7 @@ let songs = [
       "앨범/엘리니아.png",
     artist: "엘레니아",
     audio: "./노래/엘리니아마을.mp3",
-    id: 116,
+    id: 15,
     number: "15 / 59",
   },
       {
@@ -141,7 +141,7 @@ let songs = [
       "앨범/늪지대.png",
     artist: "커닝시티",
     audio: "./노래/늪지대.mp3",
-    id: 117,
+    id: 16,
     number: "16 / 59",
   },
       {
@@ -150,7 +150,7 @@ let songs = [
       "앨범/헤네시스.png",
     artist: "헤네시스",
     audio: "./노래/헤네시스_전체.mp3",
-    id: 118,
+    id: 17,
     number: "17 / 59",
   },
       {
@@ -159,7 +159,7 @@ let songs = [
       "앨범/리스항구.png",
     artist: "리스항구",
     audio: "./노래/리스항구.mp3",
-    id: 119,
+    id: 18,
     number: "18 / 59",
   },
       {
@@ -168,7 +168,7 @@ let songs = [
       "앨범/10.JPG",
     artist: "에레브",
     audio: "./노래/에레브_전체.mp3",
-    id: 120,
+    id: 19,
     number: "19 / 59",
   },
       {
@@ -177,7 +177,7 @@ let songs = [
       "앨범/16.JPG",
     artist: "엘나스",
     audio: "./노래/엘나스.mp3",
-    id: 121,
+    id: 20,
     number: "20 / 59",
   },
       {
@@ -186,7 +186,7 @@ let songs = [
       "앨범/켄타우로스의영역.png",
     artist: "에레브",
     audio: "./노래/켄타우로스의 영역.mp3",
-    id: 122,
+    id: 21,
     number: "21 / 59",
   },
       {
@@ -195,7 +195,7 @@ let songs = [
       "앨범/루디브리엄.gif",
     artist: "루디브리엄",
     audio: "./노래/루디브리엄.mp3",
-    id: 123,
+    id: 22,
     number: "22 / 59",
   },
   {
@@ -204,7 +204,7 @@ let songs = [
       "앨범/군영.png",
     artist: "페리온",
     audio: "./노래/군영.mp3",
-    id: 124,
+    id: 23,
     number: "23 / 59",
   },
   {
@@ -213,7 +213,7 @@ let songs = [
       "앨범/1.gif",
     artist: "세계여행 대만",
     audio: "./노래/1.mp3",
-    id: 0,
+    id: 24,
     number: "24 / 59",
   },
   {
@@ -222,7 +222,7 @@ let songs = [
       "앨범/2.gif",
     artist: "루디브리엄 알리샤르 파티퀘스트",
     audio: "./노래/2.mp3",
-    id: 1,
+    id: 25,
     number: "25 / 59",
   },
   {
@@ -231,7 +231,7 @@ let songs = [
       "앨범/3.gif",
     artist: "오르비스 여신의 흔적 파티퀘스트",
     audio: "./노래/3.mp3",
-    id: 2,
+    id: 26,
     number: "26 / 59",
   },
   {
@@ -240,7 +240,7 @@ let songs = [
       "앨범/4.gif",
     artist: "결혼 시스템 파티퀘스트",
     audio: "./노래/4.mp3",
-    id: 3,
+    id: 27,
     number: "27 / 59",
   },
   {
@@ -249,7 +249,7 @@ let songs = [
       "앨범/5.gif",
     artist: "엘리니아 사냥터",
     audio: "./노래/5.mp3",
-    id: 4,
+    id: 28,
     number: "28 / 59",
   },
   {
@@ -258,7 +258,7 @@ let songs = [
       "앨범/6.gif",
     artist: "루디브리엄 행",
     audio: "./노래/6.mp3",
-    id: 5,
+    id: 29,
     number: "29 / 59",
   },
   {
@@ -267,7 +267,7 @@ let songs = [
       "앨범/7.gif",
     artist: "아쿠아리움 사냥터",
     audio: "./노래/7.mp3",
-    id: 6,
+    id: 30,
     number: "30 / 59",
   },
   {
@@ -276,7 +276,7 @@ let songs = [
       "앨범/8.gif",
     artist: "아쿠아리움 사냥터",
     audio: "./노래/8.mp3",
-    id: 7,
+    id: 31,
     number: "31 / 59",
   },
   {
@@ -285,7 +285,7 @@ let songs = [
       "앨범/9.gif",
     artist: "코크타운",
     audio: "./노래/9.mp3",
-    id: 8,
+    id: 32,
     number: "32 / 59",
   },
   {
@@ -294,7 +294,7 @@ let songs = [
       "앨범/12.gif",
     artist: "루디브리엄 사냥터",
     audio: "./노래/12.mp3",
-    id: 10,
+    id: 33,
     number: "33 / 59",
   },
   {
@@ -303,7 +303,7 @@ let songs = [
       "앨범/13.gif",
     artist: "오르비스",
     audio: "./노래/13.mp3",
-    id: 11,
+    id: 34,
     number: "34 / 59",
   },
   {
@@ -312,7 +312,7 @@ let songs = [
       "앨범/14.gif",
     artist: "오르비스",
     audio: "./노래/14.mp3",
-    id: 12,
+    id: 35,
     number: "35 / 59",
   },
   {
@@ -321,7 +321,7 @@ let songs = [
       "앨범/15.gif",
     artist: "세계여행 중국",
     audio: "./노래/15.mp3",
-    id: 13,
+    id: 36,
     number: "36 / 59",
   },
   {
@@ -330,7 +330,7 @@ let songs = [
       "앨범/16.JPG",
     artist: "엘나스 사냥터",
     audio: "./노래/16.mp3",
-    id: 14,
+    id: 37,
     number: "37 / 59",
   },
   {
@@ -339,7 +339,7 @@ let songs = [
       "앨범/17.gif",
     artist: "오르비스 행",
     audio: "./노래/17.mp3",
-    id: 15,
+    id: 38,
     number: "38 / 59",
   },
   {
@@ -348,7 +348,7 @@ let songs = [
       "앨범/18.JPG",
     artist: "루디브리엄 사냥터",
     audio: "./노래/18.mp3",
-    id: 16,
+    id: 39,
     number: "39 / 59",
   },
   {
@@ -357,7 +357,7 @@ let songs = [
       "앨범/18.JPG",
     artist: "루디브리엄 사냥터",
     audio: "./노래/26.mp3",
-    id: 17,
+    id: 40,
     number: "40 / 59",
   },
   {
@@ -366,7 +366,7 @@ let songs = [
       "앨범/19.gif",
     artist: "루디브리엄 에오스탑",
     audio: "./노래/19.mp3",
-    id: 18,
+    id: 41,
     number: "41 / 59",
   },
   {
@@ -375,7 +375,7 @@ let songs = [
       "앨범/20.gif",
     artist: "루디브리엄 사냥터",
     audio: "./노래/20.mp3",
-    id: 19,
+    id: 42,
     number: "42 / 59",
   },
   {
@@ -384,7 +384,7 @@ let songs = [
       "앨범/21.JPG",
     artist: "루디브리엄 사냥터",
     audio: "./노래/21.mp3",
-    id: 20,
+    id: 43,
     number: "43 / 59",
   },
   {
@@ -393,7 +393,7 @@ let songs = [
       "앨범/22.jpg",
     artist: "판테온 마을",
     audio: "./노래/22.mp3",
-    id: 21,
+    id: 44,
     number: "44 / 59",
   },
   {
@@ -402,7 +402,7 @@ let songs = [
       "앨범/23.JPG",
     artist: "판테온 사냥터",
     audio: "./노래/23.mp3",
-    id: 22,
+    id: 45,
     number: "45 / 59",
   },
   {
@@ -411,7 +411,7 @@ let songs = [
       "앨범/59.JPG",
     artist: "판테온 마을",
     audio: "./노래/59.mp3",
-    id: 23,
+    id: 46,
     number: "46 / 59",
   },
   {
@@ -420,7 +420,7 @@ let songs = [
       "앨범/24.JPG",
     artist: "에델슈타인 사냥터",
     audio: "./노래/24.mp3",
-    id: 24,
+    id: 47,
     number: "47 / 59",
   },
   {
@@ -429,7 +429,7 @@ let songs = [
       "앨범/25.gif",
     artist: "아리안트 사냥터",
     audio: "./노래/25.mp3",
-    id: 25,
+    id: 48,
     number: "48 / 59",
   },
   {
@@ -438,7 +438,7 @@ let songs = [
       "앨범/27.gif",
     artist: "월묘 파티 퀘스트",
     audio: "./노래/27.mp3",
-    id: 26,
+    id: 49,
     number: "49 / 59",
   },
   {
@@ -447,7 +447,7 @@ let songs = [
       "앨범/28.JPG",
     artist: "시간의 신전 사냥터",
     audio: "./노래/28.mp3",
-    id: 27,
+    id: 50,
     number: "50 / 59",
   },
   {
@@ -456,7 +456,7 @@ let songs = [
       "앨범/29.JPG",
     artist: "직업 팬텀의 크리스탈 가든",
     audio: "./노래/29.mp3",
-    id: 28,
+    id: 51,
     number: "51 / 59",
   },
   {
@@ -465,7 +465,7 @@ let songs = [
       "앨범/30.gif",
     artist: "세계여행 싱가포르 (한국미출시)",
     audio: "./노래/30.mp3",
-    id: 29,
+    id: 52,
     number: "52 / 59",
   },
   {
@@ -474,7 +474,7 @@ let songs = [
       "앨범/31.JPG",
     artist: "세계여행 중국 사냥터",
     audio: "./노래/31.mp3",
-    id: 30,
+    id: 53,
     number: "53 / 59",
   },
   {
@@ -483,7 +483,7 @@ let songs = [
       "앨범/32.JPG",
     artist: "백초마을",
     audio: "./노래/32.mp3",
-    id: 31,
+    id: 54,
     number: "54 / 59",
   },
   {
@@ -492,7 +492,7 @@ let songs = [
       "앨범/33.JPG",
     artist: "니은 숲",
     audio: "./노래/33.mp3",
-    id: 32,
+    id: 55,
     number: "55 / 59",
   },
   {
@@ -501,7 +501,7 @@ let songs = [
       "앨범/34.JPG",
     artist: "세계여행 일본",
     audio: "./노래/34.mp3",
-    id: 33,
+    id: 56,
     number: "56 / 59",
   },
   {
@@ -510,7 +510,7 @@ let songs = [
       "앨범/35.jpg",
     artist: "셀라스 마을 (구.ver)",
     audio: "./노래/35.mp3",
-    id: 34,
+    id: 57,
     number: "57 / 59",
   },
   {
@@ -519,7 +519,7 @@ let songs = [
       "앨범/39.jpg",
     artist: "지구방위본부대 : 격납고",
     audio: "./노래/39.mp3",
-    id: 36,
+    id: 58,
     number: "58 / 59",
   },
  {
@@ -528,7 +528,7 @@ let songs = [
       "앨범/37.gif",
     artist: "로그인 화면 (구.ver)",
     audio: "./노래/37.mp3",
-    id: 37,
+    id: 59,
     number: "59 / 59",
   }
   
